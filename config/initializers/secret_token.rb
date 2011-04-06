7566d5765be18c986b174700bc17e44e91049d6e
=Tyler Chesley <= tylerc@mediarain.com>
2011-04-06 12:13:17 -0600

    Created Rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NdepthWebapp::Application.config.secret_token = 'b40b165ee80a19588233ed7e1434e7d2ce33852e1199d63a7266d01f9ff9ba0185d2d4e3f0a671a376eb5ffdca4a186ab08cebed8187380d493bc93b09b2df23'
